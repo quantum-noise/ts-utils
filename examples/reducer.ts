fd94b41269561a413c2f2b6e980f7cc4edd1bf60
Aliaksandr Klimau <aiklimau@gmail.com>
2020-01-24 20:36:56 +0300

    Action creators refactoring

--- examples/reducer.ts
+++ examples/reducer.ts
@@ -1,21 +1,21 @@
-import { actionCreator, composeActionHandlers, handleAction } from '../src';
+import { actionCreator, composeActionHandlers, handleAction } from '../src/action-creators';
 
 const setLoading = actionCreator<boolean>('SET_IS_LOADING');
 const setUserData = actionCreator<{ name: string }>('SET_USER_DATA');
 
-interface Domain {
+interface Store {
     user: { name: string };
     isLoading: boolean;
 }
 
-const initialState: Domain = {
+const initialState: Store = {
     user: {
         name: '',
     },
     isLoading: false,
 };
 
-export const reducer = composeActionHandlers<Domain>(
+export const reducer = composeActionHandlers<Store>(
     handleAction(setLoading, (store, payload) => ({
         ...store,
         isLoading: payload,
