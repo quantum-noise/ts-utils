e2ede48c80fb3df8e1580043bf08e0da5d547712
Aliaksandr Klimau <alexandrklimov@outlook.com>
2020-09-22 21:28:38 +0300

    Fix lensPath ts 4.1 types

--- src/lens/lens-path-ts-4_1.ts
+++ src/lens/lens-path-ts-4_1.ts
@@ -1,4 +1,4 @@
-// https://www.typescriptlang.org/play?ts=4.1.0-dev.20200921#code/C4TwDgpgBA0hJQLxQM7AE4EsB2BzKAPlNgK4C2ARhOgNwBQdokUAShAI4mboQAmbAYwA8AFQB8SKCKgQAHsAjZeKKAHsKAKwgDgdKPqgB+KAG8oAbRhQcUANbxVAM1YcuPXqLEBdAFwvO3HyCQmwB7p6WXhIAvnoGfiL0jODQADKKKCIAhui4EMBCAApZwAAWEsjFZTLyisoWOI7UUAASEFm8XnH6xm0dNQpKKvYgTlAAFI3NAMqlWZAAlN0GPVCz89Byg-Vm5iPW2K3tnYZ+U+hQAGpZADYk0LErTwbGu-s2fSd+13cPy8-6PzYCAAN2o-3ixFB4JWfiqpQGdRU5nORw6ABooAA6HGTbBNC7ZTA3BZdFa9Y6IoZ2BzOPEEqRLAFGKRUnYWd6HT5eU5QdLYTI5PIFIk3GIQ56vDnwA5or58jLZXL5URZYni5mwqFg9ASqBA6G6rXAnVJJjQADi+UK6FUYB+91EbJU6i0OkxNrtFVZWyRCoFSuFQnMnrAURZIhDtrD+qgJCUEEcOD4ZpS-pQ8KKJVKmPEknhzoa+Oa3OWxmkvup-MFyoK5m5Yj1xitwFDDogokxnwklfq8d4ieTvD15KgLbbt0doTcQW0ni7xwkRH7g+Bw81kPH0fbnbljeZfhXSbXyzh2cLKOLF0+mJxWPpzVFpLLPtqVcVQpV9eOt5xoqiTbppmoqYludo7iIC4dGIPZvn2CbHnwI4vEB2aqsSoHWtuk4dpBe6EHGCFDvuG6xtWwFqjcmGtthvy7t2JHPIeREnsahpJHIYCqOgwBQAIqgCrxNwZAWyBFIWcAgOYUTjHeYDZnCCxIBIJjLOa6ZZBQwmSNWgYqoUYj0Cs6nVhB3rkWhhS5oZDArPxglQMJAqSE6vYqNWmnCWI4yugkCx+KZOGeMpph6jwwAkOghzyWUWLuCQAgQOM4xZAIAiYiCOFKYgKnIfo4WRYcqUCIYWLmJlvxeEYxhHkORmkQA9A1MgoDcODAAAtLwmAoJ5EAdcC8gdW1wJQAAAuaKACFgYCdRArXtQ12CqB1nFtQImCdVk2AgHq0SYq6UBZCo20gAs9X6NESR2QJaBQDwAgAJIKBcYmFBJ8DSbmhautowCYpc3kxaUcIHZoCQZTh3z+ayOWhcymB0sDWJObg1QSAAjEpqmkXxt28fsyDA+YAAMVXHTSozOIktmkQVUXw7jBh3q66J5U8TUtSNnXdb1Wn9YNnUjdAE0pFNM1zQt2DAA1FDbR1wAoB1mC4MtPDsysnMTUrKtqxAGsGOYxNk7493aM91DjMjC2JeMWNgxopV7PAXiQ78Cxs7jV17TINwoNAON0-khWM0z2I4qzBv6Eb2ak14psVfcnukd7zKPAY3vLE5KBYv7vFiRWcHuRkfXeYnEABRkZkO35CQhQ9FvoFb2YO279znbT+XBwz2f0N7QA
+// https://www.typescriptlang.org/play?ts=4.1.0-dev.20200921#code/C4TwDgpgBA0hJQLxQM7AE4EsB2BzKAPlNgK4C2ARhOgNwBQdokUAShAI4mboQAmbAYwA8AFQB8SKCKgQAHsAjZeKKAHsKAKwgDgdKPqgB+KAG8oAbRhQcUANbxVAM1YcuPXqLEBdAFwvO3HyCQmwB7p6WXhIAvnoGfiL0jODQADKKKCIAhui4EMBCAApZwAAWEsjFZTLyisoWOI7UUAASEFm8XnH6xm0dNQpKKvYgTlAAFI3NAMqlWZAAlN0GPVCz89Byg-Vm5iPW2K3tnYZ+U+hQAGpZADYk0LErTwbGu-s2fSd+13cPy8-6PzYCAAN2o-3ixFB4JWfiqpQGdRU5nORw6ABooAA6HGo7KYG5dFa9Y6IoZ2BzOSbYJoXERLAFGKRknYWd6HT5eU5QdLYTI5PIFfE3GIQ56vNnwA5or48jLZXL5URZAmixmwqFg9BiqBA6HajXArVJJjQADi+UK6FUYB+91ELJU6i0OkxVptFWZWyRcr5CsFQnM7rAUSZIiD1pDuqgJCUEEcOD4JpSvpQ8KKJVKmPEknhjoaNOanOWxmk3vJvP5ioK5k5Yh1xgtwGDdogokxnwk5fqsd48cTvB1xKgTZbt3toTcQW0ng7xwkRF7-eBg-VkNHkdb7Zl9cZfiXCZXyzhmfzKMLF0+mJxWLxKsJJa9tQr8oFStrx2vOOFUQbqfTwqYhuNpbiIc4dGIXbPj2caHnwQ4vP+mbKgSQGWpu45tmBO6EDGsEDrua7RpWAH3mhzYYb826doRzz7vhR6GvqSRyGAqjoMAUACKofKcTcGR5sgRT5nAIDmFE4w3mAmZwgsSASCYyymqmWQUPxkiVv6SqFGI9ArMplagZ6JHIYU2a6QwKzcbxUD8XykgOt2KiVqp-FiOMzoJAsfiGZhnjyaYOo8MAJDoIc0llFi7gkAIEDjOMWQCAImIgphcmIApCH6MFoWHIlAiGFi5ipb8XhGMYB4DnpREAPQ1TIKA3DgwAALS8JgKCuRALXAvILVNcCUAAAKmigAhYGArUQI1zU1dgqgtaxTUCJgrVZNgIA6tEmLOlAWQqOtIALNV+jREkVk8WgUA8AIACSCgXEJhQifA4nZvmzraMAmKXO5EWlHCO2aAkKWYd83nMhlgWMpgVL-Vidm4NUEgAIxyYpRFcZdnH7Mg-3mAADGV+0UqMziJJZRE5WF0OYwYN7OuiWVPHVDUDa17WdWp3W9a1A3QCNKRjRNU0zdgwA1RQ60tcAKAtZguDzTwzMrKzI1ywrSsQCrBjmPjRO+Nd2j3dQ4zwzNsXjGjQMaIVezwF4oO-AsTOY2dW0yDcKDQBjVP5LltN09iOKMzr+h65mhNeIbJX3K7RHu4yjwGO7yx2SgWLe5xQlltBzkZF17mxxAPkZEZNteQkAU3Sb6Bm5mNtO-cx2U9l-s0+n9BnUAA
 
 type Key = string | number;
 
@@ -12,7 +12,7 @@ type LensTarget<Path> = Path extends [infer Head]
             ? { [key in Head]?: Value }
             : never
         : never
-    : Path extends [infer Head, ...(infer Tail)]
+    : Path extends [infer Head, ...infer Tail]
     ? Head extends keyof (infer T)
         ? T extends { [key in Head]?: LensTarget<Tail> }
             ? { [key in Head]?: LensTarget<Tail> }
@@ -28,7 +28,7 @@ type LensPath<Path, T> = Path extends [infer Head]
             ? GetPropValue<RequiredRec<T>, Head> | undefined
             : GetPropValue<T, Head>
         : undefined
-    : Path extends [infer Head, ...(infer Tail)]
+    : Path extends [infer Head, ...infer Tail]
     ? T extends LensTarget<[Head, ...Tail]>
         ? LensPath<Tail, GetPropValue<T, Head>> extends undefined
             ? LensPath<Tail, GetPropValue<T, Head> | undefined>
